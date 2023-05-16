d68dabeb3583a5fc36447d6a8c01694e90d114b6
LKerin822 <119484187+LKerin822@users.noreply.github.com>
2023-05-16 13:33:20 +1000

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <img src="/nana-gen.jpg" className="home-img"></img>
+
+      <h1>LOUISE KERIN</h1>
+    </div>
+  );
+};
+
+export default HomePage;
